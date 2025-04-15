69d7e4f28856baaecb623151cece4e27526e001f
sleepDuck23 <br.amorim.araujo@gmail.com>
2025-04-15 16:00:46 +0200

    Create simulators.py
    adapted simulators functions

---
+++ simulators/simulators.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+
+def CreateAdjacencyAR1(N,rho):
+    A = np.zeros((N,N))
+
+    for i in range(N):
+        for j in range(N):
+            A[i, j] = rho ** abs((i-1)-(j-1))
+
+    return A
+
+
+def GenerateSynthetic_order_p(K,A,H,p,x0,sigma_P, sigma_Q, sigma_R):
+    Ny = len(H)
+    Nx = len(H)
+
+    x = np.zeros((Nx,K))
+    y = np.zeros((Ny,K))
+
+    # Initialization of the state
+    for pp in range(p):
+        x[:, pp] = x0 + sigma_P * np.random.randn(Nx)
+
+    # Main loop
+    for k in range(p, K):
+        deterministic_state = np.zeros(Nx)
+        for pp in range(p):
+            deterministic_state += A[:, :, pp] @ x[:, k - pp - 1]
+        x[:, k] = deterministic_state + sigma_Q * np.random.randn(Nx)
+        y[:, k] = H @ x[:, k] + sigma_R * np.random.randn(Ny)
+
+    return [y,x]
